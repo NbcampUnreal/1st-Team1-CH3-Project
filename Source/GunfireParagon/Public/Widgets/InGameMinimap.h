f272975fc7fa948a708c89b7c410263a24d59c91
gwo1004 <mynameiskgw@gmail.com>
2025-02-18 19:49:56 +0900

    [Init] 인게임 미니맵 관련 Widget 생성

---
+++ Source/GunfireParagon/Public/Widgets/InGameMinimap.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InGameMinimap.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GUNFIREPARAGON_API UInGameMinimap : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
