f23258e87f5eebbd0bf0e2d18d7f26a825638d7a
Midam <121799933+GolGol00@users.noreply.github.com>
2025-02-25 18:31:03 +0900

    [Add] CSV연동 _완_에 대한 생성 커밋

--- Source/GunfireParagon/Public/FPSDataTables.h
+++ Source/GunfireParagon/Public/FPSDataTables.h
@@ -2,49 +2,32 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Dom/JsonObject.h"  // JSON 파싱용
+#include "Serialization/JsonReader.h"
+#include "Serialization/JsonSerializer.h"
 #include "Engine/DataTable.h"
 #include "FPSDataTables.generated.h"
 
 USTRUCT(BlueprintType)
-struct FWeaponDataTable : public FTableRowBase
+struct FWeaponData : public FTableRowBase
 {
 	GENERATED_BODY()
 
 public:
-	FWeaponDataTable()
-	{
-		RowName = TEXT("Weapon1");
-		Key = 1;
-		Name = TEXT("Weapon");
-		AttackPower = 10.0f;
-		AttackSpeed = 10.0f;
-		MaxAmmo = 30;
-		GunDelay = 0.1f;
-		Grade = TEXT("Normal");
-	}
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
-	FName RowName;  
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	// UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	// FName Name;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int32 Key;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
-	FName Name;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float AttackPower;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float AttackSpeed;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int32 MaxAmmo;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float GunDelay;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	FName Grade;
 };
 
@@ -60,15 +43,18 @@ protected:
 private:
 	// CSV에서 DataTable을 로드하는 함수
 	void LoadWeaponDataFromCSV();
+	void LoadWeaponDataFromJSON();
 
 
 public:
 
 	//  데이터 테이블에서 특정 무기 데이터를 가져오는 함수 추가
-	FWeaponDataTable* GetWeaponDataByKey(int32 Key);
+	FWeaponData* GetWeaponDataByKey(int32 Key);
 
 	//  블루프린트에서 설정할 수 있도록 데이터 테이블 변수 추가
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data", meta = (AllowPrivateAccess = "true"))
 	UDataTable* WeaponDataTable;
+
+	TMap<int32, FWeaponData*> WeaponDataMap;
 	
 };
